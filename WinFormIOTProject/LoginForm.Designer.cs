22bdac75e008510fde89f2a13af6e3b400f05258
Zade <muhderfannnn@gmail.com>
2021-11-17 19:32:43 +0800

    Add project files.

---
+++ WinFormIOTProject/LoginForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WinFormIOTProject
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameTxtbox = new System.Windows.Forms.TextBox();
+            this.PasswordTxtBox = new System.Windows.Forms.TextBox();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.ForgetPassBtn = new System.Windows.Forms.Button();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UsernameTxtbox
+            // 
+            this.UsernameTxtbox.Location = new System.Drawing.Point(228, 106);
+            this.UsernameTxtbox.Name = "UsernameTxtbox";
+            this.UsernameTxtbox.Size = new System.Drawing.Size(100, 20);
+            this.UsernameTxtbox.TabIndex = 0;
+            // 
+            // PasswordTxtBox
+            // 
+            this.PasswordTxtBox.Location = new System.Drawing.Point(228, 202);
+            this.PasswordTxtBox.Name = "PasswordTxtBox";
+            this.PasswordTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTxtBox.TabIndex = 1;
+            this.PasswordTxtBox.TextChanged += new System.EventHandler(this.PasswordTxtBox_TextChanged);
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(117, 106);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UsernameLabel.TabIndex = 2;
+            this.UsernameLabel.Text = "Username";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(119, 205);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password";
+            this.PasswordLabel.Click += new System.EventHandler(this.PasswordLabel_Click);
+            // 
+            // ForgetPassBtn
+            // 
+            this.ForgetPassBtn.Location = new System.Drawing.Point(122, 271);
+            this.ForgetPassBtn.Name = "ForgetPassBtn";
+            this.ForgetPassBtn.Size = new System.Drawing.Size(119, 23);
+            this.ForgetPassBtn.TabIndex = 5;
+            this.ForgetPassBtn.Text = "Forget Password";
+            this.ForgetPassBtn.UseVisualStyleBackColor = true;
+            this.ForgetPassBtn.Click += new System.EventHandler(this.ForgetPassBtn_Click);
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(276, 271);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(52, 23);
+            this.LoginBtn.TabIndex = 6;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 440);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.ForgetPassBtn);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.PasswordTxtBox);
+            this.Controls.Add(this.UsernameTxtbox);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UsernameTxtbox;
+        private System.Windows.Forms.TextBox PasswordTxtBox;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Button ForgetPassBtn;
+        private System.Windows.Forms.Button LoginBtn;
+    }
+}
+
